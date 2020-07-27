8e8bae0623ace87b3f92c1f4318b93399b5e26e6
ajasparks <52292110+ajasparks@users.noreply.github.com>
2020-07-27 10:22:58 -0400

    Add files via upload

---
+++ I Am Poor/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Aja Denae on 7/22/20.
+//  Copyright © 2020 Aja Denae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
